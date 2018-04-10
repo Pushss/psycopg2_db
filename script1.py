bd9f9d68fdb05ffd1192ec1a9a85b9b1514cf173
Pushss <chrisjamescam10@gmail.com>
2018-04-10 23:26:45 +0100

    updated comments added update function

--- script1.py
+++ script1.py
@@ -14,7 +14,7 @@ def insert(item,quantity,price): #function to insert data using insert("coffer c
         conn.commit() #commit changes
         conn.close() #close connection
 
-insert("coffer cup",3,1.5)
+insert("coffee cup",3,1.5) #insert data into db 
 
 def view(): #function to view the db in console
     conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
@@ -24,4 +24,21 @@ def view(): #function to view the db in console
     conn.close() #close conection to db
     return rows #return variable storing all data from the TABLE
 
+print(view()) #print db rows as list
+
+def delete(item): #delete date cotaining specified item
+    conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
+    cur=conn.cursor() #create cursor within db
+    cur.execute("DELETE FROM store WHERE item=?",(item,)) #Delete row containing the item e.g. delete("coffee cup")
+    conn.commit() #commit changes to db
+    conn.close() #close conection to db
+
+def update(quantity,price,item): #function to update row within TABLE
+    conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
+    cur=conn.cursor() #create cursor within db
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item =?",(quantity,price,item)) #Will update data in database where row contains the specified ITEM
+    conn.commit() #commit changes to db
+    conn.close() #close conection to db
+
+update(11,6,"Water glass") #update rows containing Water glass with new values
 print(view()) #print db rows as list
