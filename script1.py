e27f393ae84caad27a82167afcb54a1f8317985f
Pushss <chrisjamescam10@gmail.com>
2018-04-10 23:06:46 +0100

    created function to view and insert data as well as create table

--- script1.py
+++ script1.py
@@ -1,7 +1,27 @@
 import sqlite3 #import sql libary from python already preinstalled
 
-conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
-cur=conn.cursor() #create cursor within db
-cur.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)") # create table named store containing 3 rows (item, quantity, price)
-conn.commit() #commit changes
-conn.close() #close connection
+def create_table(): #function to create table if not already created
+    conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
+    cur=conn.cursor() #create cursor within db
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)") # create table named store containing 3 rows (item, quantity, price)
+    conn.commit() #commit changes
+    conn.close() #close connection
+
+def insert(item,quantity,price): #function to insert data using insert("coffer cup",3,1.5)
+        conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
+        cur=conn.cursor() #create cursor within db
+        cur.execute("INSERT INTO store VALUES (?,?,?)",(item,quantity,price)) #Insert data into values (inserted in order)
+        conn.commit() #commit changes
+        conn.close() #close connection
+
+insert("coffer cup",3,1.5)
+
+def view(): #function to view the db in console
+    conn=sqlite3.connect("lite.db") #create connection to db(pass database file)
+    cur=conn.cursor() #create cursor within db
+    cur.execute("SELECT * FROM store")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
